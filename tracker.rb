076c5808216f424685e67317aa057096a9c0a323
Ismael Celis <ismaelct@gmail.com>
2011-09-30 18:09:06 +0100

    Removed comments

--- tracker.rb
+++ tracker.rb
@@ -20,18 +20,6 @@ class Api < EM::Connection
      
   def process_http_request
     
-    # the http request details are available via the following instance variables:
-    #   @http_protocol
-    #   @http_request_method
-    #   @http_cookie
-    #   @http_if_none_match
-    #   @http_content_type
-    #   @http_path_info
-    #   @http_request_uri
-    #   @http_query_string
-    #   @http_post_content
-    #   @http_headers
-    
     @params = Rack::Utils.parse_nested_query(@http_query_string)
 
     @response = EM::DelegatedHttpResponse.new(self)
