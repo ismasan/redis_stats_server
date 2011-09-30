c74a806e4ec2d7aac90f93771f1057880cd49471
Ismael Celis <ismaelct@gmail.com>
2011-09-30 16:45:42 +0100

    Tracking dates

--- tracker.rb
+++ tracker.rb
@@ -13,11 +13,11 @@ require File.join(File.dirname(__FILE__), 'mapper')
 class Api < EM::Connection
   include EM::HttpServer
 
-   def post_init
-     super
-     no_environment_strings
-   end
-
+  def post_init
+    super
+    no_environment_strings
+  end
+     
   def process_http_request
     
     # the http request details are available via the following instance variables:
@@ -33,12 +33,13 @@ class Api < EM::Connection
     #   @http_headers
     
     @params = Rack::Utils.parse_nested_query(@http_query_string)
+
     @response = EM::DelegatedHttpResponse.new(self)
-    p [:serving, @http_path_info]
+    
     # Router
     case @http_path_info
-    when /\/(.+)\/_utm\.gif/
-      handle_hit $1
+    when /\/track\.gif/
+      handle_hit
     when /^\/r/
       serve_graph
     else
@@ -49,8 +50,10 @@ class Api < EM::Connection
   
   protected
   
-  def handle_hit(hit_key)
-    # /:hit_key/_utm.gif?y=2011&mm=09&d=30&t=11&m=54&h=foo.com&p=/home&e=new_user
+  TIME_KEYS = [:year, :month, :day, :min]
+  
+  def handle_hit
+    # /track.gif?y=2011&mm=09&d=30&t=11&m=54&h=foo.com&p=/home&e=new_user
     #
     # a       account_key
     # y       year
@@ -59,17 +62,28 @@ class Api < EM::Connection
     # m       minute
     # h       host
     # 
+    # Store UTC time, compare with client-provided tz (timezone offset)
+    now = Time.now.getutc
     
-    # Store session id in all passed params with prefixes
-    @params.each do |prefix, val| # y, 2011 | m, 03
-      REDIS.sadd("#{prefix}:#{val}", hit_key)
+    # sadd 'y:2011', 'some_uid'
+    # sadd 'm:10', 'some_uid' 
+    TIME_KEYS.each do |period|
+      REDIS.sadd("#{period}:#{now.send(period)}", unique_hit_id)
     end
-    p [:handle_hit]
+    p [:track, @params]
+    # Store unique id in all passed params with prefixes
+    @params.each do |prefix, val| # y, 2011 | m, 03
+      p [:foo, prefix, val]
+      REDIS.sadd("#{prefix}:#{val}", unique_hit_id) unless TIME_KEYS.include?(prefix.to_sym)
+    end
+    
     # That's it! Don't even wait for redis responses
     @response.content_type 'image/gif'
     @response.status = 200
-    @response.headers['Cache-Control'] = 'no-cache' # browsers should not cache this
-    @response.content = ''
+    @response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, private' # browsers should not cache this
+    @response.headers['Pragma'] = 'no-cache'
+    @response.headers['Expires'] = 'no-cache'
+    @response.content = 'Fri, 24 Nov 2000 01:00:00 GMT'
     @response.send_response
   end
   
@@ -92,6 +106,17 @@ class Api < EM::Connection
     @response.status = 404
     @response.content = ''
     @response.send_response
+  end
+  
+  def remote_ip
+    @remote_ip ||= (
+      @http_headers =~ /X-Forwarded-For: (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/
+      $1
+    )
+  end
+  
+  def unique_hit_id
+    @unique_hit_id ||= [remote_ip, (Time.now.to_f * 1000).to_i].join(':')
   end
 
 end
